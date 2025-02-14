7a9c940f2d46d05fa1ee1fc54c48a508e9f76aa6
Daniel Joo <danieljoo@Daniels-MacBook-Pro.local>
2025-02-14 14:27:47 -0500

    Implemented basic error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/daniel-joo-dsj/learn-golang/greetings"
 )
 
 
 func main() {
+	// Set log properties
+	log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+	// Request a greeting message
 	var message string
-	message = greetings.Hello("Daniel")
+	var err error
+	message, err = greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error, print message
 	fmt.Println(message)
 }
